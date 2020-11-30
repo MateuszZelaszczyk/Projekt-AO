7a8c8023765123b90ad18e8578db3ac1cee39812
Mateusz Zelaszczyk <mateusz.zelaszczyk@gmail.com>
2020-11-30 23:06:12 +0100

    dodanie id oraz name do pól formularza

--- register_panel.php
+++ register_panel.php
@@ -6,33 +6,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="register.css" type="text/css" />
-    <SCRIPT type="text/javascript" language="JavaScript" SRC="scripts.js"></SCRIPT>
+    <SCRIPT type="text/javascript" language="JavaScript" SRC="formularz.js"></SCRIPT>
     <title>Rejestracja</title>
 </head>
 
 <body>
-    <div class="container-form" >
-        <form class="register-form" action="Dodaj.php" method="POST">
+    <div class="container-form">
+        <form class="register-form" id="form" action="Dodaj.php" method="POST">
             <H2>Panel rejestracji</H2>
-            <label>Login:*</label> <input class="inp" name="Login" required="">
+            <label>Login(minimum 4 znaki):*</label> <input class="inp" id="log" name="Login" required="">
             <br>
             <label>E-mail:*</label><input type="email" class="inp" name="Mail" required="">
             <br>
-            <label>Hasło:*</label><input type="password" class="inp" name="Haslo" required="">
+            <label>Hasło:*</label><input type="password" class="inp" id="haslo" name="Haslo" required="">
             <br>
-            <label>Powtórz hasło:*</label><input type="password" name="haslo2" class="inp" required="">
+            <label>Powtórz hasło:*</label><input type="password" id="haslo2" class="inp" required="">
             <br>
-            <label>Nr telefonu:*</label><input type="tel" class="inp" name="Telefon" required="">
+            <label>Nr telefonu(format: 111-111-111):*</label><input type="tel" class="inp" id="Nr" name="Telefon" required="">
             <br>
             <label>Data urodzenia:*</label><input type="date" class="inp" name="Urodziny" required="">
             <br>
             <div class="btn-box">
-                <button class="btn">Załóż konto</button>
+                <button type="submit" class="btn">Załóż konto</button>
                 <input type="button" class="btn" onclick="location.href='./index.html';" value="Cofnij">
             </div>
         </form>
     </div>
-    
+
 </body>
 
 </html>
