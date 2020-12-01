1fbe6ffe2fdd8b151f8c608f1e7091e837938014
Mateusz Zelaszczyk <mateusz.zelaszczyk@gmail.com>
2020-12-01 18:39:19 +0100

    dodano PobierzDane.php

---
+++ PobierzDane.php
@@ -0,0 +1,17 @@
+<?php
+$login=$_POST["Login"];
+$haslo=$_POST["Haslo"];
+    $connection = @mysqli_connect('127.0.0.1:3306', 'root', '', 'baza')
+        or die('zły adres lub login');
+    mysqli_set_charset($connection, 'UTF8');
+    $sql = "select * from `baza uzytkownikow`";
+    $wynik = mysqli_query($connection, $sql);
+    $jest=false;
+    while ($linia = mysqli_fetch_array($wynik)){
+       if($linia[1]==$login && $linia[3]==$haslo){
+           $jest=true;
+       }
+    }
+    $data[2]=$jest;
+    echo json_encode($data);
+    ?>
